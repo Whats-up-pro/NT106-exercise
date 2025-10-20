75c02231e158da3269e1928af7af9721fcfd3a24
614_2U0C <147789234+Whats-up-pro@users.noreply.github.com>
2025-10-20 22:01:22 +0700

    Remove Lab3 folder from tracking

--- Lab3/run-chat-client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo    BAI 8: CHAT ROOM - CLIENT
-echo ========================================
-echo.
-cd /d "%~dp0Bai8_ChatRoom_Client"
-echo Starting Chat Client...
-dotnet run
-pause
--- Lab3/run-chat-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo    BAI 8: CHAT ROOM - SERVER
-echo ========================================
-echo.
-cd /d "%~dp0Bai8_ChatRoom_Server"
-echo Starting Chat Server...
-dotnet run
-pause
